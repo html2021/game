e63510e764d0a0a856c8edee44e56d1daedc7a44
html2021 <78289310+html2021@users.noreply.github.com>
2023-10-07 18:47:56 +0900

    Add files via upload

---
+++ gacha.js
@@ -0,0 +1,16 @@
+function gacha() {
+    let randomNumber2 = Math.random(); //確率
+    if (randomNumber2 < 0.5) {
+        console.log(randomNumber2)
+        console.log("☆一");
+    } else if (randomNumber2 < 0.85) {
+        console.log(randomNumber2)
+        console.log("☆二");
+     } else {
+        console.log(randomNumber2)
+        console.log("☆三");
+    }
+}
+function set(){
+    
+}
